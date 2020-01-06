a5621b221022eb510f47b01570da92220a0d2eb5
Christopher Floyd <cfloyd252@gmail.com>
2020-01-06 14:28:38 -0700

    moddified drill.js

--- drill.js
+++ drill.js
@@ -1,4 +1,6 @@
 function createGreeting(){
-    console.log('Hi, my name is Chris and I'm 29 years old');
+    return "Hi, my name is Chris and I'm 29 years old";
 };
-createGreeting();
+
+const greeting1 = createGreeting();
+console.log(greeting1);
