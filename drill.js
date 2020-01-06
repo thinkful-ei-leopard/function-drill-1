e2a8881668a53daa4ea7d4e6b50d302301184828
Charles <charlesdpulliam@gmail.com>
2020-01-06 15:49:25 -0600

    getYearOfBirth function

--- drill.js
+++ drill.js
@@ -1,6 +1,10 @@
+function getYearOfBirth(age){
+    return 2020 - age;
+};
+
 function createGreeting(name, age){
     const yearOfBirth = 2020 - age;
-    return `I was born in ${yearOfBirth}`;
+    return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${yearOfBirth}`;
 };
 
 const greeting1 = createGreeting();
