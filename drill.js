043c12e03c8b2c0f250d0ec1d7902a4fa67fbe66
Charles <charlesdpulliam@gmail.com>
2020-01-06 15:33:45 -0600

    added arguments

--- drill.js
+++ drill.js
@@ -1,5 +1,5 @@
-function createGreeting(){
-    return "Hi, my name is Chris and I'm 29 years old";
+function createGreeting(name, age){
+    return `Hi, my name is ${name} and I'm ${age} years old`;
 };
 
 const greeting1 = createGreeting();
